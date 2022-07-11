db30d0ac4f203e5f95fd515b4ae82156395be9ed
surtarso <tarso.galvao@gmail.com>
2022-07-11 18:17:34 -0300

    code refactor

--- Projeto - Snake Game/scripts.js
+++ Projeto - Snake Game/scripts.js
@@ -1,69 +1,120 @@
 
-canv=document.getElementById("gc");
-ctx=canv.getContext("2d");
 document.addEventListener("keydown",keyPush);
-setInterval(game,1000/15);
 
-px=py=10;
-gs=tc=20;
-ax=ay=15;
-xv=yv=0;
-trail=[];
-tail = 5;
+CANVAS = document.getElementById("gc");
+CONTEXT = CANVAS.getContext("2d");
 
+CANVAS.width = 400;
+CANVAS.height = 400;
 
+gs = tc = 20;
 
-function game() {
-    px+=xv;
-    py+=yv;
-    if(px<0) {
-        px= tc-1;
+//snake and apple start position
+snake_x = snake_y = 10;
+apple_x = apple_y = 15;
+//tail initial size
+snake_tail_initial = 3
+snake_tail = snake_tail_initial;
+trail = [];
+//move direction (stopped)
+vec_x = vec_y = 0;
+snake_speed = 2.5
+
+setInterval(updateGame,1000/snake_speed);
+
+function updateGame() {
+    snake_x += vec_x;
+    snake_y += vec_y;
+
+    setDirection();
+    drawBrackground();
+    drawSnake();
+    drawApple();
+}
+
+function setDirection(){
+    if(snake_x < 0) {
+        snake_x = tc - 1;
     }
-    if(px>tc-1) {
-        px= 0;
+    if(snake_x > tc - 1) {
+        snake_x = 0;
     }
-    if(py<0) {
-        py= tc-1;
+    if(snake_y < 0) {
+        snake_y = tc - 1;
     }
-    if(py>tc-1) {
-        py= 0;
+    if(snake_y > tc - 1) {
+        snake_y = 0;
     }
-    ctx.fillStyle="black";
-    ctx.fillRect(0,0,canv.width,canv.height);
- 
-    ctx.fillStyle="lime";
-    for(var i=0;i<trail.length;i++) {
-        ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
-        if(trail[i].x==px && trail[i].y==py) {
-            tail = 5;
+}
+
+//background
+function drawBrackground(){
+    CONTEXT.fillStyle = "black";
+    CONTEXT.fillRect(
+            0,
+            0,
+            CANVAS.width,
+            CANVAS.height
+    );
+}
+
+//snake
+function drawSnake(){
+    CONTEXT.fillStyle = "lime";
+    for(var i = 0; i < trail.length; i++) {
+        CONTEXT.fillRect(
+                trail[i].x * gs,
+                trail[i].y * gs,
+                gs - 2,
+                gs - 2
+        );
+
+        //eat own body
+        if(trail[i].x ==snake_x && trail[i].y == snake_y) {
+            snake_tail = snake_tail_initial;
         }
     }
-    trail.push({x:px,y:py});
-    while(trail.length>tail) {
-    trail.shift();
+
+    trail.push({x:snake_x, y:snake_y});
+
+    while(trail.length > snake_tail) {
+        trail.shift();
     }
- 
-    if(ax==px && ay==py) {
-        tail++;
-        ax=Math.floor(Math.random()*tc);
-        ay=Math.floor(Math.random()*tc);
+
+    //eat apple
+    if(apple_x == snake_x && apple_y == snake_y) {
+        snake_tail++;
+        apple_x = Math.floor(Math.random() * tc);
+        apple_y = Math.floor(Math.random() * tc);
     }
-    ctx.fillStyle="red";
-    ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
 }
+
+//apple
+function drawApple(){
+    CONTEXT.fillStyle = "red";
+    CONTEXT.fillRect(
+        apple_x * gs,
+        apple_y * gs,
+        gs - 2,
+        gs - 2
+    );
+}
+
+
+//move keys
 function keyPush(evt) {
     switch(evt.keyCode) {
         case 37:
-            xv=-1;yv=0;
+            vec_x = -1; vec_y = 0;
             break;
         case 38:
-            xv=0;yv=-1;
+            vec_x = 0; vec_y = -1;
             break;
         case 39:
-            xv=1;yv=0;
+            vec_x = 1; vec_y = 0;
             break;
         case 40:
-            xv=0;yv=1;
+            vec_x = 0; vec_y = 1;
             break;
     }
 }
