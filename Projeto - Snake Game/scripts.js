7f7c8bff30569860b82fb97a362e0567ea605f47
surtarso <tarso.galvao@gmail.com>
2022-07-13 23:24:26 -0300

    added touch keys, code refactor

--- Projeto - Snake Game/scripts.js
+++ Projeto - Snake Game/scripts.js
@@ -1,6 +1,5 @@
 CANVAS = document.getElementById("gameCanvas");
 CONTEXT = CANVAS.getContext("2d");
-SCORE = document.getElementById("score");
 SNAKE_SIZE = document.getElementById("size");
 
 //size relation
@@ -17,19 +16,19 @@ snake_trail = [];
 
 score = 0
 
-//move direction (stopped)
+//initial move direction (stopped)
 x_velocity = y_velocity = 0;
-snake_speed = 4
-
+snake_speed = 5
+//keyboard keys listener
 document.addEventListener("keydown",keyPush);
 setInterval(updateGame,1000/snake_speed);
 
+//----------------------------------------MAIN UPDATE:
 function updateGame() {
     //move
     snake_x += x_velocity;
     snake_y += y_velocity;
     //update score/size
-    SCORE.innerHTML = "Score: " + score;
     SNAKE_SIZE.innerHTML = "Size: " + snake_tail;
 
     wrapScreen();
@@ -37,6 +36,7 @@ function updateGame() {
     drawApple();
     drawSnake();
 }
+
 //screen wrap
 function wrapScreen(){
     if(snake_x < 0) {
@@ -52,6 +52,8 @@ function wrapScreen(){
         snake_y = 0;
     }
 }
+
+//------------------------------------- DRAW ITEMS:
 //background
 function drawBrackground(){
     CONTEXT.fillStyle = "black";
@@ -62,16 +64,7 @@ function drawBrackground(){
             CANVAS.height
     );
 }
-//apple
-function drawApple(){
-    CONTEXT.fillStyle = "red";
-    CONTEXT.fillRect(
-        apple_x * grid_size,
-        apple_y * grid_size,
-        grid_size - 2,
-        grid_size - 2
-    );
-}
+
 //snake
 function drawSnake(){
     CONTEXT.fillStyle = "lime";
@@ -82,11 +75,17 @@ function drawSnake(){
                 grid_size - 2,
                 grid_size - 2
         );
-
+        
+        //------------------ LOSE CONDITION:
         //eat own body
         if(snake_trail[i].x ==snake_x && snake_trail[i].y == snake_y) {
+            //reset to initial settings
             snake_tail = snake_tail_initial;
             score = 0
+            x_velocity = y_velocity = 0;
+            //snake and apple start position
+            snake_x = snake_y = 10;
+            apple_x = apple_y = 15;
         }
     }
 
@@ -104,21 +103,36 @@ function drawSnake(){
     }
 }
 
+//apple
+function drawApple(){
+    CONTEXT.fillStyle = "red";
+    CONTEXT.fillRect(  //make this a circle later
+        apple_x * grid_size,
+        apple_y * grid_size,
+        grid_size - 2,
+        grid_size - 2
+    );
+}
+
+//-------------------------------------------- PLACE NEW APPLE:
 //prevents apple from spawning on snake body
 function placeNewApple(){
     apple_x = Math.floor(Math.random() * tile_count);
     apple_y = Math.floor(Math.random() * tile_count);
-    for(let i = 0; i < snake_trail.length; i++){
-        if(apple_x == snake_trail[i].x || apple_y == snake_trail[i].y){
-            placeApple()
-        } else {
-            return apple_x, apple_y
+    for(let i = 0; i <= snake_trail.length; i++){
+        if(apple_x != snake_trail[i].x || apple_y != snake_trail[i].y){
+            console.log('placed')
+            return apple_x, apple_y;
+        } 
+        else {
+            console.log('hit');
+            placeNewApple(); 
         }
     }
 }
 
-
-//move keys
+//--------------------------------------------------MOVEMENT:
+//keyboard move keys
 function keyPush(evt) {
     switch(evt.keyCode) {
         case 37:
@@ -142,4 +156,26 @@ function keyPush(evt) {
             }
             break;
     }
+}
+
+//touch move keys
+function l() {
+    if(x_velocity != 1){
+        x_velocity = -1; y_velocity = 0;
+    }
+}
+function u() {
+    if(y_velocity != 1){
+        x_velocity = 0; y_velocity = -1;
+        }
+}
+function r() {
+    if(x_velocity != -1){
+        x_velocity = 1; y_velocity = 0;
+    }
+}
+function d() {
+    if(y_velocity != -1){
+        x_velocity = 0; y_velocity = 1;
+        }
 }
